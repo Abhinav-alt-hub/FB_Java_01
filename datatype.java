1361d02fcf2e43d035aada474b593374642f95ed
Abhinav_Dubey <abhinavdubey1302@gmail.com>
2025-08-20 18:42:52 +0530

    Add files via upload

---
+++ datatype.java
@@ -0,0 +1,10 @@
+public class datatype {
+    public static void main(String[] args) {
+        System.out.println(Byte.MIN_VALUE + " to " + Byte.MAX_VALUE);
+        System.out.println(Integer.MIN_VALUE + " to " + Integer.MAX_VALUE);
+        System.out.println(Long.MIN_VALUE + " to " + Long.MAX_VALUE);
+        System.out.println(Float.MIN_VALUE + " to " + Float.MAX_VALUE);
+        System.out.println(Double.MIN_VALUE + " to " + Double.MAX_VALUE);
+        
+    }
+}

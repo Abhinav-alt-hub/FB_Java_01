9aa48373d73c38cc6e9645680334442de7a1c073
Abhinav_Dubey <abhinavdubey1302@gmail.com>
2025-08-20 18:43:14 +0530

    Add files via upload

---
+++ Reverse.java
@@ -0,0 +1,14 @@
+public class Reverse {
+public static void main(String[] args) {
+    int number = 12345; 
+    int reversednumber = 0;
+    while (number !=0){
+        int digit = number % 10;
+        reversednumber = reversednumber * 10 + digit;
+        number = number/10;
+
+    }
+    System.out.println("Reversed Number: " + reversednumber);
+}
+}
+

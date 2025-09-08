bb1a838710a030b9aa3e5b17c59ff55d5e433e8d
Abhinav_Dubey <abhinavdubey1302@gmail.com>
2025-09-08 12:49:18 +0530

    Add files via upload

---
+++ Assignment/CountDigitsinaNumber.java
@@ -0,0 +1,12 @@
+public class CountDigitsinaNumber {
+    public static void main(String[] args) {
+        int number = 98765; 
+        int count = 0;
+        int temp = number;
+        while (temp != 0) {
+            temp /= 10;
+            count++;
+        }
+        System.out.println("Number of digits in " + number + " is: " + count);
+    }
+}

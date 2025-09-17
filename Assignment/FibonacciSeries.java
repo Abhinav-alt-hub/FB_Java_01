1d69b0f23530d648331d88704c99b76dae3add7f
Abhinav_Dubey <abhinavdubey1302@gmail.com>
2025-09-17 20:33:23 +0530

    Add files via upload

---
+++ Assignment/FibonacciSeries.java
@@ -0,0 +1,13 @@
+public class FibonacciSeries {
+    public static void main(String[] args) {
+        int n =  7; 
+        int a = 0, b = 1;
+        System.out.println("Fibonacci series up to " + n + " terms:");
+        for (int i = 1; i <= n; i++) {
+            System.out.print(a + " ");
+            int next = a + b;
+            a = b;
+            b = next;
+        }
+    }
+}
